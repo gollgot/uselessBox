dc815718088f3c53b5ecb5c87b223861d8ca5494
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-06-24 12:19:02 +0200

    Update src code to the v2 version with random behaviors

--- src/uselessBox/uselessBox.ino
+++ src/uselessBox/uselessBox.ino
@@ -1,35 +1,82 @@
 #include <Servo.h>
 
+// constants won't change. They're used here to set pin numbers:
+const int MOTOR_LED = 7;    
+const int TENSION_LED = 4; 
+const int BUTTON =  2;      
+const int MOTOR = 9;
+const int MAX_ANGLE = 100;
+Servo servo; 
 
-const int BUTTON = 2; 
-const int LED =  13; 
-const int SERVO = 9;
-
-int btnState = 0;
-Servo servo;
+// variables will change:
+int btnState = 0;         // variable for reading the pushbutton status
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(LED, OUTPUT);
+  // initialize the MOTOR_LED pin as an output:
+  pinMode(MOTOR_LED, OUTPUT);
+  pinMode(TENSION_LED, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(BUTTON, INPUT);
-  // Initialize the servo motor
-  servo.attach(SERVO);
+  servo.attach(MOTOR);
 }
 
 void loop() {
   // read the state of the pushbutton value:
   btnState = digitalRead(BUTTON);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  digitalWrite(TENSION_LED, HIGH);
+  // check if the pushbutton is pressed. If it is, the btnState is HIGH:
   if (btnState == HIGH) {
-    // turn LED on and move the servo from 0 to 180 degree
-    digitalWrite(LED, HIGH);
-    servo.write(90);
-    
-  }else {
-    // turn LED off and servo to 0 degree
-    digitalWrite(LED, LOW);
+    // turn MOTOR_LED on:
+    digitalWrite(MOTOR_LED, HIGH);
+    doBehaviour(random(1,4));
+  } else {
+    // turn MOTOR_LED off:
+    digitalWrite(MOTOR_LED, LOW);
     servo.write(0);
+  }
+}
+
+
+void doBehaviour(int nb){
+  switch(nb) {
+    case 0:
+      behaviour0();
+      break;
+    case 1:
+      behaviour1();
+      break;
+    case 2:
+      behaviour2();
+      break;
+    default:
+      behaviour0();
+      break;
+  }
+}
+
+void behaviour0(void) {
+  while(btnState != LOW){
+    btnState = digitalRead(BUTTON);
+    servo.write(MAX_ANGLE);
+  }
+}
+
+void behaviour1(void){
+  servo.write(45);
+  delay(500);
+  servo.write(0);
+  delay(1000);
+  while(btnState != LOW){
+    btnState = digitalRead(BUTTON);
+    servo.write(MAX_ANGLE);
+  }
+}
+
+void behaviour2(void){
+  servo.write(35);
+  delay(1000);
+  while(btnState != LOW){
+    btnState = digitalRead(BUTTON);
+    servo.write(MAX_ANGLE);
   }
 }
